e7e460dbc3bc1c6fd9272c4ea5945dd3fadd8fd1
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-01 10:56:15 -0500

    added Signup component, passing props from app.js

---
+++ src/scenes/Signup.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export default function Signup({ setToken, setIsUser }) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+        .then(response => response.json())
+        .then(data => setToken(data.token))
+        .catch(alert)
+    }
+    return (
+        <>
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Email:
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            </label><br/>
+            <label>Password:
+                <input type= "password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <input type="submit" value="Sign up" />
+        </form>
+        <button onClick={() => setIsUser(true)}>Login</button>
+        </>
+    )
+}
