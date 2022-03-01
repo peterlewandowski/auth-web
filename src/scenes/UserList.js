df1f9262d864b4996a69f71dba9395dbae2fc0d3
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-01 14:47:25 -0500

    using JWT for getUsers

--- src/scenes/UserList.js
+++ src/scenes/UserList.js
@@ -3,7 +3,13 @@ import { useEffect, useState } from 'react'
 export default function UserList({ token }) {
     const [userList, setUserList] = useState()
     useEffect(() => {
-        fetch('http://localhost:3001/users') // was missing a : 
+        fetch('http://localhost:3001/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+            }
+        }) // was missing a : 
             .then(response => response.json())
             .then(data => setUserList(data.users))
             .catch(alert)
